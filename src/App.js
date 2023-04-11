909b4b9e46464f43f5473240ca6ccc550bd4d563
Gustavo <gustavo.okamoto@unicesumar.edu.br>
2023-04-11 19:01:57 -0300

    App.js atualizado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+  const [valor, setValor] = useState();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Seja bem-vindo!</h2>
+      <form>
+        <input onChange={(e) => setValor(e.target.value)}></input>
+      </form>
+
+      <p>{valor}</p>
     </div>
   );
 }
